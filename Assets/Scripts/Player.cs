b95740a126501dd35398d8dc332e34f7cf78b458
Masahiro Nitta <83905521+nittamas@users.noreply.github.com>
2026-07-05 21:51:38 +0900

    プレイヤーにRigidbody、コライダー、向き制御、アニメーションを追加

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,22 +1,29 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] float speedMax;
+    [SerializeField] float accel;
+    [SerializeField] float rotateSpeed;
+    [SerializeField] Animator animator;
+    [SerializeField] float jumpSpeed;
 
     PlayerInput playerInput;
+    Rigidbody rb;
+    Vector3 rotateTarget;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        var moveVec = playerInput.actions["Move"].ReadValue<Vector2>();
+        var accelVec = playerInput.actions["Move"].ReadValue<Vector2>();
 
         var cameraDir = playerInput.camera.transform.forward;
         cameraDir.y = 0;
@@ -24,9 +31,37 @@ public class Player : MonoBehaviour
 
         var cameraRight = playerInput.camera.transform.right;
 
-        var moveVec3D =
-            cameraDir * moveVec.y * speedMax
-            + cameraRight * moveVec.x * speedMax;
-        transform.position = transform.position + moveVec3D * Time.deltaTime;
+        var accelVec3D =
+            cameraDir * accelVec.y * accel
+            + cameraRight * accelVec.x * accel;
+        rb.AddForce(accelVec3D, ForceMode.Acceleration);
+
+        // プレイヤーの向きを変える
+        if(accelVec3D != Vector3.zero)
+        {
+            rotateTarget = accelVec3D.normalized;
+        }
+
+        // 前方向をコピーしておく
+        Vector3 forward = transform.forward;
+
+        // 上方向を固定
+        transform.up = Vector3.up;
+
+        // 前方向をターゲットに向かって補間
+        transform.forward =
+            Vector3.Slerp(forward, rotateTarget, rotateSpeed * Time.deltaTime);
+
+        // アニメーターのMoveSpeedパラメータに Rigidbody の移動速度の大きさを与える
+        Vector3 velocityXZ = rb.linearVelocity;
+        velocityXZ.y = 0;
+        animator.SetFloat("MoveSpeed", velocityXZ.magnitude);
+
+        // ジャンプ
+        if (playerInput.actions["Jump"].WasPressedThisFrame())
+        {
+            Vector3 jumpVec = new Vector3(0, jumpSpeed, 0);
+            rb.AddForce(jumpVec, ForceMode.VelocityChange);
+        }
     }
 }
